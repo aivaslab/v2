2e6bb6ad0f619fe3375b0f99d40a71b3a2aa8d6c
tengyum <tengyu.ma@vanderbilt.edu>
2020-03-31 14:42:15 -0500

    update gitignore

--- v2/util/conf.py
+++ v2/util/conf.py
@@ -3,7 +3,7 @@ import os
 HOME_DIR = os.getenv('HOME')
 MEDIA_DATA_DIR = '/media/tengyu/DataU'
 DATA_DIR = os.path.join(MEDIA_DATA_DIR, 'Data')
-PJ_ROOT = os.path.dirname(os.path.abspath(__file__))
+PJ_ROOT = '/home/tengyu/Documents/research/v2/v2'
 
 CKPT_DIR = os.path.join(PJ_ROOT, 'exp', 'ckpt')
 RST_DIR = os.path.join(PJ_ROOT, 'exp', 'rst')
@@ -11,6 +11,7 @@ RST_DIR = os.path.join(PJ_ROOT, 'exp', 'rst')
 ModelNet_DIR = os.path.join(DATA_DIR, 'ModelNet')
 ModelNet10_DIR = os.path.join(ModelNet_DIR, 'ModelNet10')
 ModelNet40_DIR = os.path.join(ModelNet_DIR, 'ModelNet40')
+ModelNet40OBJ_DIR = os.path.join(ModelNet_DIR, 'ModelNet40_OBJ')
 ModelNet10FacesNP_DIR = os.path.join(ModelNet_DIR, 'ModelNet10FacesNP')
 ModelNet40FacesNP_DIR = os.path.join(ModelNet_DIR, 'ModelNet40FacesNP')
 ModelNet10_c10000_DIR = os.path.join(ModelNet_DIR, 'ModelNet10_c10000')
